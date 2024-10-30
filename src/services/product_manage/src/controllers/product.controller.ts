bd3278b3d5387e2d2729f7ad18ca3115e1eb3183
aquino alejandro <aquinoalejandro666@gmail.com>
2024-10-30 10:42:27 -0300

    feat: Product Manage!

--- src/services/product_manage/src/controllers/product.controller.ts
+++ src/services/product_manage/src/controllers/product.controller.ts
@@ -11,10 +11,13 @@ export class ProductController {
             price,
             description
         }        
+
+        await ProductModel.create(product);
     }
 
     async updateProduct(req: any, res: any) {
         const { id } = req.params;
+
         const { name, category, price, description } = req.body;
 
         const product = {
@@ -22,15 +25,20 @@ export class ProductController {
             category,
             price,
             description
-        }        
+        }
+
+        await ProductModel.update(product, { where: { id } })
     }
 
     async deleteProduct(req: any, res: any) {
         const { id } = req.params;
+
+        await ProductModel.destroy({ where: { id } })
     }
 
     async getProducts(req: any, res: any) {
         const products = await ProductModel.findAll();
+        
         res.json(products);
     }
 }
