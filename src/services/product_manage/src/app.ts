196960da224984189b07ecfd9af8e5ee6cbb5ac4
Fabian <garciafabianemmanuel@gmail.com>
2024-10-30 11:07:31 -0300

    feat: :sparkles: Craecion de la tabla productos

--- src/services/product_manage/src/app.ts
+++ src/services/product_manage/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import {productRoutes} from './routes/product.routes';
 import { envs } from './environments/environments';
+import { connectionDB } from './config/connectionDB';
 
 const  { PORT } = envs
 
@@ -9,6 +10,12 @@ const app = express();
 app.use(express.json());
 app.use('/api',productRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Server on http://localhost:${PORT}`);
-});
+async function productServer() {
+    await connectionDB();
+
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+} 
+
+productServer();
