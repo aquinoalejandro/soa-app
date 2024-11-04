7cb060fa9e5bcdf5cfc045e0a254538fcd7d6f04
solchuuxx <natalistevens1@hotmail.com>
2024-11-04 11:03:40 -0300

    feat: :sparkles: Docker File

--- src/services/product_manage/src/environments/environments.ts
+++ src/services/product_manage/src/environments/environments.ts
@@ -1,12 +1,11 @@
 
-import "dotenv/config";
 import * as env from "env-var";
 import { Dialect } from 'sequelize';
 
 
 
 export const envs = {
-    PORT: env.get('PORT').required().asPortNumber(),
+    PORT: 3000,
     DB_HOST: env.get('DB_HOST').required().asString(),
     DB_PORT: env.get('DB_PORT').required().asPortNumber(),
     DB_USER: env.get('DB_USER').required().asString(),
